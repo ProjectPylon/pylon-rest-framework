e86b00f630a8178f66a3cb92a47168e2a7b7ab2b
Dummyc0m <m.steve.jobs@me.com>
2017-03-07 11:57:59 +0800

    new Wheel()

--- src/main/kotlin/com/dummyc0m/pylon/framework/experimental/PylonServiceManager.kt
+++
@@ -1,7 +0,0 @@
-package com.dummyc0m.pylon.framework.experimental
-
-/**
- * Created by dummy on 2/28/17.
- */
-class PylonServiceManager {
-}
---
+++ src/main/kotlin/com/dummyc0m/pylon/framework/experimental/microservice/registry.kt
@@ -0,0 +1,5 @@
+package com.dummyc0m.pylon.framework.experimental.microservice
+
+/**
+ * Created by dummy on 3/7/17.
+ */
