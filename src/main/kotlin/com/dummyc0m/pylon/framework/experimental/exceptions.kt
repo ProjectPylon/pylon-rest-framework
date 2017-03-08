7a6f112fc5a1aac28b314cb6075bcb4d8d82dc86
Dummyc0m <m.steve.jobs@me.com>
2017-03-08 09:41:07 +0800

    Trigger

--- src/main/kotlin/com/dummyc0m/pylon/framework/experimental/exceptions.kt
+++ src/main/kotlin/com/dummyc0m/pylon/framework/experimental/exceptions.kt
@@ -1,6 +1,7 @@
 package com.dummyc0m.pylon.framework.experimental
 
 /**
+ * buncho exceptions, probably not useful at all.
  * Created by dummy on 3/7/17.
  */
 class ServiceNotFoundException(message: String?) : RuntimeException(message)
